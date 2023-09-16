428d9e3bdaf45705c0f67e28581c051de5159bdc
Diegolvp <diego.pievan123@gmail.com>
2023-09-16 12:32:18 -0300

    dssdf
    sdfsdf

--- assets/js/minhasviagens.js
+++ assets/js/minhasviagens.js
@@ -4,12 +4,20 @@ countstar = document.querySelector(".count_star")
 stars.forEach((star, index1) => {
     star.addEventListener("mouseover", () => {
         stars.forEach((star, index2) => {
+            if(star.parentNode.classList == 'stars'){
             index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
-
-            let count = document.querySelectorAll(".stars .active")
-            countstar.textContent = count.length +".0"
-
+            }else{
+                star.removeEventListener("mouseover", ()=>{}, false)
+            }
         });
     });
+
+    star.addEventListener("click" , () => {
+        star.parentNode.classList.remove("stars")
+        star.parentNode.classList.add("stars-selected")
+    })
 });
+
+var viagensCount = document.querySelector(".viagens_count")
+viagensCount.textContent = document.querySelectorAll(".gd_viagens-item").length + " Viagens"
 
